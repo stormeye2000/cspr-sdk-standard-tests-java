1fa32ae4b79fafdb7ec4de7ac2d2ea0d5aa5809d
cnorburn <carl.norburn@gmail.com>
2023-03-13 10:48:00 +0000

    add putDeploy to test block transfers

--- src/test/java/com/stormeye/evaluation/EvaluateBlocks.java
+++ src/test/java/com/stormeye/evaluation/EvaluateBlocks.java
@@ -2,14 +2,39 @@ package com.stormeye.evaluation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.casper.sdk.exception.NoSuchTypeException;
+import com.casper.sdk.helper.CasperTransferHelper;
 import com.casper.sdk.identifier.block.HashBlockIdentifier;
 import com.casper.sdk.identifier.block.HeightBlockIdentifier;
 import com.casper.sdk.model.block.JsonBlockData;
 import com.casper.sdk.model.common.Digest;
+import com.casper.sdk.model.common.Ttl;
+import com.casper.sdk.model.deploy.Deploy;
+import com.casper.sdk.model.deploy.DeployData;
+import com.casper.sdk.model.deploy.DeployResult;
+import com.casper.sdk.model.event.EventTarget;
+import com.casper.sdk.model.event.EventType;
+import com.casper.sdk.model.key.PublicKey;
 import com.casper.sdk.model.transfer.TransferData;
 import com.casper.sdk.service.CasperService;
+import com.casper.sdk.service.EventConsumer;
+import com.casper.sdk.service.EventService;
+import com.syntifi.crypto.key.Ed25519PrivateKey;
 
+import com.casper.sdk.model.event.Event;
+
+import java.io.IOException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Random;
+import dev.oak3.sbs4j.exception.ValueSerializationException;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -20,17 +45,23 @@ public class EvaluateBlocks {
     private static JsonBlockData blockData;
     private static final String url = "localhost";
     private static final Integer port = 11101;
-    protected static CasperService casperServiceNctl;
+    protected static CasperService csprServiceNctl;
+
+    protected static EventService eventService;
+
+    protected String getResourcesKeyPath(String filename) throws URISyntaxException {
+        return Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource(filename)).toURI()).toString();
+    }
 
     @BeforeAll
     public static void setUp() throws MalformedURLException {
-        casperServiceNctl = CasperService.usingPeer(url, port);
+        csprServiceNctl = CasperService.usingPeer(url, port);
         blockData = null;
     }
 
     @Given("that the latest block is requested")
     public void thatTheLatestBlockIsRequested() {
-        blockData = casperServiceNctl.getBlock();
+        blockData = csprServiceNctl.getBlock();
     }
 
     @Then("a valid block is returned")
@@ -79,26 +110,63 @@ public class EvaluateBlocks {
 
     @Given("that a block is returned by hash")
     public void thatABlockIsReturnedByHash() {
-        final JsonBlockData latestBlock = casperServiceNctl.getBlock();
+        final JsonBlockData latestBlock = csprServiceNctl.getBlock();
         final String hash = latestBlock.getBlock().getHash().toString();
 
-        blockData = casperServiceNctl.getBlock(new HashBlockIdentifier(hash));
+        blockData = csprServiceNctl.getBlock(new HashBlockIdentifier(hash));
     }
 
     @Given("that a block is returned by height {int}")
     public void thatABlockIsReturnedByHeight(long height) {
-        blockData = casperServiceNctl.getBlock(new HeightBlockIdentifier(height));
+        blockData = csprServiceNctl.getBlock(new HeightBlockIdentifier(height));
     }
 
     @Given("that a transfer block is requested")
-    public void thatATransferBlockIsRequested() {
+    public void thatATransferBlockIsRequested() throws NoSuchTypeException, GeneralSecurityException, ValueSerializationException, URISyntaxException, IOException {
 
-        final TransferData blockTransfers = casperServiceNctl.getBlockTransfers();
+        DeployResult result = doTransfer();
+
+        DeployData deploy = csprServiceNctl.getDeploy(result.getDeployHash());
+
+        final TransferData blockTransfers = csprServiceNctl.getBlockTransfers();
 
     }
 
-    private void doTransfer(){
+    private DeployResult doTransfer() throws URISyntaxException, IOException, NoSuchTypeException, GeneralSecurityException, ValueSerializationException {
 
+        Ed25519PrivateKey user1 = new Ed25519PrivateKey();
+        Ed25519PrivateKey user2 = new Ed25519PrivateKey();
+
+        user1.readPrivateKey(getResourcesKeyPath("assets/users/user-1/secret_key.pem"));
+        user2.readPrivateKey(getResourcesKeyPath("assets/users/user-2/secret_key.pem"));
+
+        long id = Math.abs(new Random().nextInt());
+        Ttl ttl = Ttl
+                .builder()
+                .ttl("30m")
+                .build();
+        Ed25519PrivateKey from = user1;
+        PublicKey to = PublicKey.fromAbstractPublicKey(user2.derivePublicKey());
+
+        Deploy deploy = CasperTransferHelper.buildTransferDeploy(from, to,
+                BigInteger.valueOf(2500000000L), "casper-net-1",
+                id, BigInteger.valueOf(100000000L), 1L, ttl, new Date(),
+                new ArrayList<>());
+
+
+        DeployResult deployResult = csprServiceNctl.putDeploy(deploy);
+
+
+        do {
+            DeployData deploy1 = csprServiceNctl.getDeploy(deployResult.getDeployHash());
+
+            if (!deploy1.getExecutionResults().isEmpty()){
+                break;
+            }
+
+        } while(true);
+
+        return deployResult;
 
     }
 
